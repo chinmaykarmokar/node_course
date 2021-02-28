29409d22b8635b73dcc02ad6d07df0bfacecf2d9
chinmay <chinmaykarmokar@gmail.com>
2021-03-01 00:57:52 +0530

    First Commit

---
+++ notes-app/app.js
@@ -0,0 +1,19 @@
+const text = require('./notes.js');
+
+const result = text('Your Notes...');
+//const val = text('Your pages...');
+
+console.log(result);
+
+
+//const file = require('fs');
+
+// const add = require('./utils.js');
+
+// const sum = add(4,2);
+
+// console.log(sum);
+
+//file.writeFileSync('notes.txt','I am Chinmay!');
+
+//file.appendFileSync('notes.txt',' I am 18 years old.');
---
+++ notes-app/notes.js
@@ -0,0 +1,9 @@
+const getNotes = function (text) {
+	return text;
+}
+
+// const getNotes1 = function (note) {
+// 	return note;
+// }
+
+module.exports = getNotes;
